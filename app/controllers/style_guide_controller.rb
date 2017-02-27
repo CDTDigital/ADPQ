8cc0c6b72f868e67442733886c318675921a497f
Aurora Hay <aurora.hay@agile6.com>
2017-02-26 19:41:24 -0800

    style guide

---
+++ app/controllers/style_guide_controller.rb
@@ -0,0 +1,27 @@
+class StyleGuideController < ApplicationController
+  layout 'style_guide_layout'
+
+  def index
+    redirect_to action: 'buttons_iconography'
+  end
+
+  def colors
+
+  end
+
+  def buttons_iconography
+
+  end
+
+  def typography
+
+  end
+
+  def forms
+
+  end
+
+  def tables
+
+  end
+end
